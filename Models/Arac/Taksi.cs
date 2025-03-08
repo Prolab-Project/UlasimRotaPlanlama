961726a5fcffa009eee441692bf1c0454a7b2fda
bedirhaneren <163941707+bedirhaneren@users.noreply.github.com>
2025-03-08 16:48:43 +0300

    openingFee and costPerKm read from json

--- Models/Arac/Taksi.cs
+++ Models/Arac/Taksi.cs
@@ -4,20 +4,35 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Text.Json; 
 namespace UlasimRotaPlanlama.Models.Arac
 {
-    internal class Taksi : Arac
+    internal class Taksi : Arac, Durak
     {
         public Taksi() { }
-        double acilisUcreti = 10;
-        double kmUcreti = 4;
+        public double openingFee;
+        public double costPerKm; 
+        public void KonumBilgisi()
+        {
+            string DosyaOku;
+            DosyaOku = File.ReadAllText("dataset/bedirhan.json");
+
+            JsonDocument doc = JsonDocument.Parse(DosyaOku);
+            JsonElement root = doc.RootElement;
+
+            JsonElement taxi = root.GetProperty("taxi");
+
+            openingFee = taxi.GetProperty("openingFee").GetDouble();
+            costPerKm = taxi.GetProperty("costPerKm").GetDouble();
+
+            Console.WriteLine(openingFee + "," + costPerKm);
+        }
         public override double UcretHesapla(double mevcut_lat, double mevcut_lon, double hedef_lat , double hedef_lon)
         {
             Console.WriteLine("Mevcut konum: " + mevcut_lat + mevcut_lon); 
             double mesafe = Haversine(mevcut_lat, mevcut_lon , hedef_lat, hedef_lon);
             Console.WriteLine ("Gidilmesi gereken mesafe " + mesafe);
-            double ucret = acilisUcreti + (kmUcreti * mesafe);
+            double ucret = openingFee + (costPerKm * mesafe);
             Console.WriteLine("Odenecek ucret : " + ucret);
             return ucret; 
 
