e825b184e04c5ec0338d70c9f0124ba5a6fe52aa
OmerAttal <omarattal01@gmail.com>
2025-03-08 11:55:14 +0300

    bus_umuttepe lat lon

--- Models/Arac/Otobus/bus_umuttepe.cs
+++ Models/Arac/Otobus/bus_umuttepe.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
+using System.ComponentModel.DataAnnotations;
 
 namespace UlasimRotaPlanlama.Models.Arac.Otobus
 {
-    class bus_umuttepe
+    class bus_umuttepe : Durak
     {
+        public double lat;
+        public double lon; 
+        public void KonumBilgisi()
+        {
+            string DosyaOku;
+            DosyaOku = File.ReadAllText("C:\\Users\\Ömer\\Desktop\\moovit\\bedirhan.json");
+
+            JsonDocument doc = JsonDocument.Parse(DosyaOku);
+            JsonElement root = doc.RootElement;
+
+            lat = root.GetProperty("durak").EnumerateArray().ElementAt(3).GetProperty("lat").GetDouble();
+            lon = root.GetProperty("durak").EnumerateArray().ElementAt(3).GetProperty("lon").GetDouble();
+        }
     }
 }
