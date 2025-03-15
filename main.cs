09eeb1007aec8e2df40e54bcbcb4cd67879c921b
OmerAttal <omarattal01@gmail.com>
2025-03-15 12:06:45 +0300

    Fixed wrong json pull

--- main.cs
+++ main.cs
@@ -3,37 +3,33 @@ using System.IO;
 using Microsoft.VisualBasic.Logging;
 using System.Text.Json;
 using System.Security.Cryptography.X509Certificates;
-using UlasimRotaPlanlama.Models.Arac.Otobus;
 using UlasimRotaPlanlama.Models.Arac;
 
 namespace UlasimRotaPlanlama
 {
-   /* {
-            string DosyaOku;
-    DosyaOku = File.ReadAllText("dataset/bedirhan.json");
-
-            using JsonDocument doc = JsonDocument.Parse(DosyaOku);
-            JsonElement root = doc.RootElement;
-
-    id = root.GetProperty("duraklar").EnumerateArray().ElementAt(5).GetProperty("id").GetString();
-    name = root.GetProperty("duraklar").EnumerateArray().ElementAt(5).GetProperty("name").GetString();
-    type = root.GetProperty("duraklar").EnumerateArray().ElementAt(5).GetProperty("type").GetString();
-    sonDurak = root.GetProperty("duraklar").EnumerateArray().ElementAt(5).GetProperty("sonDurak").GetBoolean();
-    lat = root.GetProperty("duraklar").EnumerateArray().ElementAt(5).GetProperty("lat").GetDouble();
-    lon = root.GetProperty("duraklar").EnumerateArray().ElementAt(5).GetProperty("lon").GetDouble();
-
-    Console.WriteLine("id: " + id);
-            Console.WriteLine("name: " + name);
-            Console.WriteLine("type: " + type);
-            Console.WriteLine("son Durak: " + sonDurak);
-            Console.WriteLine("lat: " + lat);
-            Console.WriteLine("lon: " + lon);*/
     internal static class main
     {
 
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
+        static string JsonCekme(int x)
+        {
+            string json = File.ReadAllText("dataset/bedirhan.json");
+
+            JsonDocument doc = JsonDocument.Parse(json);
+            JsonElement root = doc.RootElement;
+
+            double lat;
+            double lon;
+            string id;
+
+            lat = root.GetProperty("duraklar").EnumerateArray().ElementAt(x).GetProperty("lat").GetDouble();
+            lon = root.GetProperty("duraklar").EnumerateArray().ElementAt(x).GetProperty("lon").GetDouble();
+            id = root.GetProperty("duraklar").EnumerateArray().ElementAt(x).GetProperty("id").GetString();
+
+            string data = $"{lat} , {lon} , {id}";
+
+            return data;
+        }
+
         [STAThread]
         static void Main()
         {
@@ -55,17 +51,15 @@ namespace UlasimRotaPlanlama
 
             //Console.WriteLine(costPerKm + "," + openingFee);
 
-            double lat_konum = Convert.ToDouble(Console.ReadLine());
-            double lon_konum = Convert.ToDouble(Console.ReadLine());
+            //double lat_konum = Convert.ToDouble(Console.ReadLine());
+            //double lon_konum = Convert.ToDouble(Console.ReadLine());
             
             Taksi taksi = new Taksi();
-            taksi.MesafeHesaplama(40.75359, 29.95328 , 40.78259, 29.94628);
-            Console.WriteLine(taksi.taksi_bin);
-            taksi.UcretHesapla();
-            taksi.DurakBilgisi();
+            taksi.MesafeHesaplama(40.75359, 29.95328, 40.78259, 29.94628);
+            //taksi.UcretHesapla();
+            //taksi.DurakBilgisi();
 
-
-            //Otobus kngof = new Otobus();
+            JsonCekme(1);
             
         }
     }
